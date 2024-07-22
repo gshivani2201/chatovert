8f473668e36490c6dbabc5f233733ab8f2f80349
Shivani Gupta <gupta.shivani7898@gmail.com>
2024-07-22 20:38:36 +0530

    get my groups API

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,13 +1,16 @@
 import express from "express";
 
 import { isAuthenticated } from "../middlewares/auth.js";
-import { getMyChats, newGroupChat } from "../controllers/chat.js";
+import { addMembers, getMyChats, getMyGroups, newGroupChat } from "../controllers/chat.js";
 
 const app = express.Router();
 
 app.use(isAuthenticated);
 
-app.post("/new", newGroupChat)
-app.get("/my", getMyChats)
+app.post("/new", newGroupChat);
+app.get("/my", getMyChats);
+app.get("/my/groups", getMyGroups);
+
+app.put("/addmembers", addMembers);
 
 export default app;
