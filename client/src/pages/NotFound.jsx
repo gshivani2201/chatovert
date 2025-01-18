633bc854c3916706b27f5664b0793d2de9073ee0
Shivani Gupta <gupta.shivani7898@gmail.com>
2025-01-18 18:35:07 +0530

    designed 404 page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,7 +1,24 @@
-// import React from "react";
+import { Link } from "react-router-dom";
+
+import { Error as ErrorIcon } from "@mui/icons-material";
+import { Container, Stack, Typography } from "@mui/material";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <Container maxWidth="lg" sx={{ height: "100vh" }}>
+      <Stack
+        alignItems={"center"}
+        justifyContent={"center"}
+        spacing={"1.5rem"}
+        height={"100%"}
+      >
+        <ErrorIcon sx={{ fontSize: "5rem", color: "red" }} />
+        <Typography variant="h1">404</Typography>
+        <Typography variant="h3">Not Found</Typography>
+        <Link to="/">Go back to home</Link>
+      </Stack>
+    </Container>
+  );
 };
 
 export default NotFound;
